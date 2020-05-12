7de2a723a4c6b7768dbb3a1d15da12aab2298c63
SamiMD <samimd.77@gmail.com>
2020-05-13 04:23:48 +0430

    Add base socket transfer object classes

---
+++ SecurityProject0_shared/SocketModels/SocketClientCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecurityProject0_shared.SocketModels
+{
+    public abstract class SocketClientCommand : SocketCommand
+    {
+    }
+}
---
+++ SecurityProject0_shared/SocketModels/SocketCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecurityProject0_shared.SocketModels
+{
+    public abstract class SocketCommand
+    {
+        public CommandType Command { get; set; }
+        public DateTime Time { get; set; }
+
+        public enum CommandType
+        {
+            Message,
+            AddUser,
+            RemoveUser,
+            Init,
+            Id,
+            Disconnect
+        }
+    }
+}
